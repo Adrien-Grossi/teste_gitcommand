1dba0e09e8e1547ae7614c1dd57fb9ceb24c413b
Adrien-Grossi <43069391+Adrien-Grossi@users.noreply.github.com>
2019-11-11 13:51:55 +0100

    second commit with site

---
+++ _site/js/index.js
@@ -0,0 +1,21 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("bm"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "js/data.json"
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("bmtow"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "js/datatow.json"
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("bmthree"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "js/datathree.json"
+})
